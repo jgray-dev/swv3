9749ff61f509e614eddb3831fdefc435276321e6
Jackson Gray <nohaxjustdoge@gmail.com>
2024-10-29 19:56:23 +0000

    Remove console.log in Details component

--- app/components/Details.tsx
+++ app/components/Details.tsx
@@ -4,7 +4,7 @@ import {LoaderData} from "~/.server/interfaces";
 export function Details() {
   let allData = useRouteLoaderData<LoaderData>("routes/_index");
   if (!allData) return null
-  console.log(allData)
+  // console.log(allData)
   return (
     <div>
       Details component here. Lat: {allData.lat}, Lon: {allData.lon}
