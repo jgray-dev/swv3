9c93a13dc43b6edab2d0599c308bd17ba6c3c507
Jackson Gray <nohaxjustdoge@gmail.com>
2024-10-29 21:15:38 +0000

    Adjust LocationDisplay div for improved responsiveness

--- app/components/LocationDisplay.tsx
+++ app/components/LocationDisplay.tsx
@@ -6,5 +6,5 @@ export default function LocationDisplay() {
   let allData = useRouteLoaderData<LoaderData>("routes/_index");
   if (!allData) return null;
 
-  return (<div className={"text-center text-2xl md:text-3xl w-screen mx-auto h-16 text-white"}>{allData.city}</div>)
+  return (<div className={"text-center text-2xl md:text-3xl min-h-fit w-screen mx-auto h-16 text-white"}>{allData.city}</div>)
 }
