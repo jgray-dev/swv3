1cc768f3b295027ccd9b71773d6094c9c561547e
Jackson Gray <nohaxjustdoge@gmail.com>
2024-10-30 01:52:15 +0000

    Enhance RatingDisplay with new layout and StatItem component

--- app/components/RatingDisplay.tsx
+++ app/components/RatingDisplay.tsx
@@ -1,6 +1,7 @@
 import { useRouteLoaderData } from "@remix-run/react";
 import { LoaderData } from "~/.server/interfaces";
 import { useEffect, useState, useRef } from "react";
+import StatItem from "~/components/StatItem";
 
 export default function RatingDisplay() {
   let allData = useRouteLoaderData<LoaderData>("routes/_index");
@@ -11,7 +12,6 @@ export default function RatingDisplay() {
   const startTimeRef = useRef<number>(0);
   const startValueRef = useRef(displayNumber);
   const ANIMATION_DURATION = 500;
-
   const radius = 40;
   const circumference = 2 * Math.PI * radius;
   const progress = (rating / 100) * circumference;
@@ -27,7 +27,8 @@ export default function RatingDisplay() {
       const progress = Math.min(elapsed / ANIMATION_DURATION, 1);
 
       if (progress < 1) {
-        const newValue = startValueRef.current + (rating - startValueRef.current) * progress;
+        const newValue =
+          startValueRef.current + (rating - startValueRef.current) * progress;
         setDisplayNumber(newValue);
         animationFrameId = requestAnimationFrame(animateNumber);
       } else {
@@ -41,7 +42,7 @@ export default function RatingDisplay() {
       cancelAnimationFrame(animationFrameId);
     };
   }, [rating]);
-  
+
   const getColor = (rating: number) => {
     if (rating <= 10) return "stroke-[#991b1b]";
     if (rating <= 30) return "stroke-[#dc2626]";
@@ -52,41 +53,70 @@ export default function RatingDisplay() {
   };
 
   return (
-    <div className={"w-screen flex justify-center"}>
-      <div className={"w-full md:w-80 md:mt-4 mt-8 mx-4 min-h-32 rounded-md flex flex-row"}>
-        <div className={"aspect-square m-4 relative"}>
-          <svg className="w-full h-full -rotate-90">
+    <div className="w-screen flex justify-center">
+      <div className="w-full bg-white/10 backdrop-blur-sm border border-white/20 sm:w-[36rem] sm:mt-4 mt-8 mx-4 min-h-32 rounded-lg flex flex-col sm:flex-row">
+        <div className="sm:basis-1/3 h-40 sm:h-auto sm:aspect-square mx-2 my-3 sm:mx-3 sm:my-4 relative flex flex-col items-center justify-center">
+          <svg
+            className="w-32 h-32 sm:w-2/3 sm:h-2/3 -rotate-90"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="xMidYMid meet"
+          >
             {rating < 100 && (
               <circle
-                cx="50%"
-                cy="50%"
+                cx="50"
+                cy="50"
                 r={radius}
                 fill="none"
                 stroke="black"
-                strokeWidth="8"
+                strokeWidth="2"
                 strokeDasharray="4,4"
-                className="opacity-20"
+                className="opacity-10"
               />
             )}
             <circle
-              cx="50%"
-              cy="50%"
+              cx="50"
+              cy="50"
               r={radius}
               fill="none"
-              strokeWidth="8"
-              className={`${getColor(rating)} transition-all duration-500 ease-in-out`}
+              strokeWidth="4"
+              className={`${getColor(
+                rating
+              )} transition-all duration-500 ease-in-out`}
               strokeDasharray={circumference}
               strokeDashoffset={dashOffset}
               strokeLinecap="round"
             />
           </svg>
-          <div className="absolute inset-0 flex items-center justify-center">
-            <span className="text-2xl font-bold">
+          <div className="absolute flex items-center justify-center">
+            <span className="text-2xl sm:text-4xl font-bold">
               {Math.round(displayNumber)}
             </span>
           </div>
+          <span className="text-sm sm:text-base mt-1 absolute bottom-0">
+            {allData.eventType.charAt(0).toUpperCase() +
+              allData.eventType.slice(1)}
+          </span>
         </div>
-        <div className={"w-full h-full bg-green-400"}>stats:</div>
+        <div className="flex-1 sm:basis-2/3 border-t sm:border-t-0 sm:border-l border-white/10 p-4 space-y-2">
+          <h3 className="text-lg font-medium text-slate-100 mb-4">
+            Condition Details
+          </h3>
+          <StatItem label="Cloud Cover" value={allData.stats.cloud_cover} />
+          <StatItem label="High Clouds" value={allData.stats.high_clouds} />
+          <StatItem label="Mid Clouds" value={allData.stats.mid_clouds} />
+          <StatItem label="Low Clouds" value={allData.stats.low_clouds} />
+          <StatItem
+            label="Visibility"
+            value={allData.stats.visibility}
+            isVisibility={true}
+          />
+          <StatItem
+            label="Temperature"
+            value={allData.stats.temperature}
+            maxValue={122}
+            unit="°F"
+          />
+        </div>
       </div>
     </div>
   );
