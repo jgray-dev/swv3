ad33ee8cde6696b462795caaa582227c202b88df
Jackson Gray <nohaxjustdoge@gmail.com>
2024-10-30 23:46:36 +0000

    Update LocationDisplay: Check for allData.ok instead of allData

--- app/components/LocationDisplay.tsx
+++ app/components/LocationDisplay.tsx
@@ -4,7 +4,7 @@ import {LoaderData} from "~/.server/interfaces";
 export default function LocationDisplay() {
 
   let allData = useRouteLoaderData<LoaderData>("routes/_index");
-  if (!allData) return null;
+  if (!allData?.ok) return null;
 
   return (<div className={"text-center text-2xl md:text-3xl min-h-fit w-screen mx-auto h-16 text-white"}>{allData.city}</div>)
 }
