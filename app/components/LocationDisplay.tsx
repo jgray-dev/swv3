03fb9195a30adcc280fdf38a367b4b37de2d777d
Jackson Gray <nohaxjustdoge@gmail.com>
2024-10-31 02:06:10 +0000

    Enhance accessibility: Add role and aria-label to LocationDisplay

--- app/components/LocationDisplay.tsx
+++ app/components/LocationDisplay.tsx
@@ -1,10 +1,19 @@
-import {useRouteLoaderData} from "@remix-run/react";
-import {LoaderData} from "~/.server/interfaces";
+import { useRouteLoaderData } from "@remix-run/react";
+import { LoaderData } from "~/.server/interfaces";
 
 export default function LocationDisplay() {
-
   let allData = useRouteLoaderData<LoaderData>("routes/_index");
   if (!allData?.ok) return null;
 
-  return (<div className={"text-center text-2xl md:text-3xl min-h-fit w-screen mx-auto h-16 text-white"}>{allData.city}</div>)
+  return (
+    <div
+      className={
+        "text-center text-2xl md:text-3xl min-h-fit w-screen mx-auto h-16 text-white"
+      }
+      role="heading"
+      aria-label={`Current city: ${allData.city}`}
+    >
+      {allData.city}
+    </div>
+  );
 }
