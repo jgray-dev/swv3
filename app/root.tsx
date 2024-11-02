690e63015e045f59eb51408873b3b4a4a913abc7
Jackson Gray <nohaxjustdoge@gmail.com>
2024-11-02 02:58:38 +0000

    Format: Improve indentation and spacing in root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type {LinksFunction} from "@remix-run/cloudflare";
+import type { LinksFunction } from "@remix-run/cloudflare";
 import {
   Links,
   Meta,
@@ -20,26 +20,29 @@ export const links: LinksFunction = () => [
   },
 ];
 
-export function Layout({children}: { children: React.ReactNode }) {
+export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-    <head>
-      <meta charSet="utf-8"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-      <script src={"https://challenges.cloudflare.com/turnstile/v0/api.js"} defer></script>
-      <Meta/>
-      <Links/>
-    </head>
-    <body className={"text-white"}>
-    {children}
-    <ScrollRestoration/>
-    <Scripts/>
-    </body>
+        <script
+          src={"https://challenges.cloudflare.com/turnstile/v0/api.js"}
+          defer
+        ></script>
+        <Meta />
+        <Links />
+      </head>
+      <body className={"text-white"}>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
     </html>
   );
 }
 
 export default function App() {
-  return <Outlet/>;
+  return <Outlet />;
 }
