cf4f7ee240ca90a0153e47d13ba30ad6d137813d
Jackson Gray <nohaxjustdoge@gmail.com>
2024-11-05 01:48:58 +0000

    Update upload table schema with proper id and time field types

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,11 +1,11 @@
-import {blob, integer, sqliteTable, text} from "drizzle-orm/sqlite-core";
+import {integer, sqliteTable, text} from "drizzle-orm/sqlite-core";
 
 
 export const uploads = sqliteTable("uploads", {
-  id: integer("id").primaryKey().notNull(),
+  id: integer("id").primaryKey({ autoIncrement: true }),
   lat: integer("lat").notNull(),
   lon: integer("lon").notNull(),
   rating: integer("rating").notNull(),
   image_url: text("image_url").notNull(),
-  time: blob("time", { mode: 'bigint' }).notNull()
+  time: integer("time").notNull()
 });
