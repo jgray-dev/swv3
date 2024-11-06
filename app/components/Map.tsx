43569a6fcbb91cee66bb3b010bba6e04053fe972
Jackson Gray <nohaxjustdoge@gmail.com>
2024-11-06 03:47:19 +0000

    feat: add image overlay and click handlers to map markers

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,39 +1,75 @@
 import { useRouteLoaderData } from "@remix-run/react";
 import { Map, Marker, Overlay, ZoomControl } from "pigeon-maps";
-import { LoaderData } from "~/.server/interfaces";
-import { useState } from "react";
+import { dbUpload, LoaderData } from "~/.server/interfaces";
+import { useEffect, useState } from "react";
 
 export default function MapComponent() {
   const allData = useRouteLoaderData<LoaderData>("routes/_index");
   const [currentZoom, setCurrentZoom] = useState<number>(9);
+  const [selectedSubmission, setSelectedSubmission] = useState<dbUpload | null>(
+    null
+  );
+  const [currentLocation, setCurrentLocation] = useState<number[]>(
+    allData?.lat && allData?.lon
+      ? [allData.lat, allData.lon]
+      : [40.7128, -74.006]
+  );
+  useEffect(() => {
+    console.log(selectedSubmission);
+  }, [selectedSubmission]);
+
+  useEffect(() => {
+    setCurrentLocation(
+      allData?.lat && allData?.lon
+        ? [allData.lat, allData.lon]
+        : [40.7128, -74.006]
+    );
+  }, [allData]);
+
   if (!allData?.ok) return null;
-  console.log(currentZoom);
   return (
-    <div
-      className="w-full h-[400px] md:h-[600px] rounded-lg overflow-hidden shadow-lg"
-      role="region"
-      aria-label="Interactive location map"
-    >
-      <Map
-        center={[allData.lon, allData.lat]}
-        animate={true}
-        defaultCenter={[allData.lat, allData.lon]}
-        defaultZoom={9}
-        onBoundsChanged={({ zoom }) => {
-          setCurrentZoom(zoom);
-        }}
+    <div className={"max-w-screen flex justify-center mx-2"}>
+      <div
+        className="md:w-1/2 w-full h-[400px] md:h-[600px] rounded-lg overflow-hidden shadow-lg"
+        role="region"
+        aria-label="Interactive location map"
       >
-        <ZoomControl />
-        {currentZoom > 10
-          ? allData.uploads.map((sub) => (
-              <Overlay anchor={[sub.lat, sub.lon]} key={sub.time} offset={[64, 60]}>
-                <img src={sub.image_url} alt={sub.city} className={"max-w-32 aspect-auto"}/>
-              </Overlay>
-            ))
-          : allData.uploads.map((sub) => (
-              <Marker anchor={[sub.lat, sub.lon]} key={sub.time}></Marker>
-            ))}
-      </Map>
+        <Map
+          // @ts-ignore
+          center={currentLocation}
+          animate={true}
+          defaultCenter={[allData.lat, allData.lon]}
+          defaultZoom={9}
+          onBoundsChanged={({ zoom }) => {
+            setCurrentZoom(zoom);
+          }}
+        >
+          <ZoomControl />
+          {currentZoom > 10
+            ? allData.uploads.map((sub) => (
+                <Overlay
+                  anchor={[sub.lat, sub.lon]}
+                  key={sub.time}
+                  offset={[64, 64]}
+                >
+                  <button onClick={() => setSelectedSubmission(sub)}>
+                    <img
+                      src={`https://imagedelivery.net/owAW_Q5wZODBr4c43A0cEw/${sub.image_id}/thumbnail`}
+                      alt={sub.city}
+                      className={"max-w-32 aspect-auto rounded-lg"}
+                    />
+                  </button>
+                </Overlay>
+              ))
+            : allData.uploads.map((sub) => (
+                <Marker
+                  anchor={[sub.lat, sub.lon]}
+                  key={sub.time}
+                  onClick={() => setSelectedSubmission(sub)}
+                />
+              ))}
+        </Map>
+      </div>
     </div>
   );
 }
