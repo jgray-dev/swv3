11bd8555f283b5b82558898f8a70458ea8d95073
Jackson Gray <nohaxjustdoge@gmail.com>
2024-11-06 05:44:38 +0000

    chore(schema): add data field to uploads table

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,4 +1,4 @@
-import { integer, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import {blob, integer, real, sqliteTable, text} from "drizzle-orm/sqlite-core";
 
 export const uploads = sqliteTable("uploads", {
   id: integer("id").primaryKey({ autoIncrement: true }),
@@ -8,4 +8,5 @@ export const uploads = sqliteTable("uploads", {
   image_id: text("image_id").notNull(),
   time: integer("time").notNull(),
   city: text("city").notNull(),
+  data: text("data").notNull()
 });
