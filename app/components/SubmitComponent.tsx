335ab4aa14c48a75dab4883ef7906725e6d8e3cc
Jackson Gray <nohaxjustdoge@gmail.com>
2024-11-12 01:14:32 +0000

    refactor: disable historic image toggle with explanatory tooltip

--- app/components/SubmitComponent.tsx
+++ app/components/SubmitComponent.tsx
@@ -105,12 +105,13 @@ export default function SubmitComponent() {
             </div>
           </>
         )}
-        <label className="inline-flex items-center me-5 cursor-pointer">
+        <label className="inline-flex items-center me-5 cursor-not-allowed" title={"Uploading historic images is current disabled."}>
           <input
             type="checkbox"
             defaultChecked={true}
             className="sr-only peer"
             name={"historic"}
+            disabled
           />
           <div className="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
           <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
