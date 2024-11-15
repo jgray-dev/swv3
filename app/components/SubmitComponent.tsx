cfd07ad6a289a24775cea1c384e04e78393bb704
Jackson Gray <nohaxjustdoge@gmail.com>
2024-11-15 04:03:41 +0000

    feat: add relative time warnings and future event validation

--- app/components/SubmitComponent.tsx
+++ app/components/SubmitComponent.tsx
@@ -6,6 +6,8 @@ import {
 } from "@remix-run/react";
 import React, { useEffect, useRef, useState } from "react";
 import { LoaderData } from "~/.server/interfaces";
+import {IoIosWarning} from "react-icons/io";
+import {PiWarningCircle, PiWarningCircleThin} from "react-icons/pi";
 
 interface ActionData {
   success: boolean;
@@ -61,10 +63,10 @@ export default function SubmitComponent() {
               relative overflow-hidden rounded-lg
               border transition-all duration-200 ease-in-out
               ${
-              selectedFile
-                ? "bg-white/20 border-white/30"
-                : "bg-white/10 border-white/20 hover:bg-white/15"
-            }
+                selectedFile
+                  ? "bg-white/20 border-white/30"
+                  : "bg-white/10 border-white/20 hover:bg-white/15"
+              }
             `}
           >
             <input
@@ -87,8 +89,7 @@ export default function SubmitComponent() {
                 stroke="currentColor"
                 viewBox="0 0 24 24"
               >
-                <path
-                  d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"/>
+                <path d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
               </svg>
               <span className="text-sm text-slate-100">
                 {selectedFile || "Choose an image"}
@@ -98,12 +99,13 @@ export default function SubmitComponent() {
         </div>
 
         {/* Hidden Fields */}
-        <input type="hidden" name="useToday" value={String(useToday)}/>
-        <input type="hidden" name="rating" value={allData.rating}/>
-        <input type="hidden" name="lat" value={allData.lat}/>
-        <input type="hidden" name="lon" value={allData.lon}/>
-        <input type="hidden" name="city" value={allData.city}/>
-        <input type="hidden" name="element" value="userSubmission"/>
+        <input type="hidden" name="eventType" value={allData.eventType} />
+        <input type="hidden" name="eventTime" value={allData.eventTime} />
+        <input type="hidden" name="rating" value={allData.rating} />
+        <input type="hidden" name="lat" value={allData.lat} />
+        <input type="hidden" name="lon" value={allData.lon} />
+        <input type="hidden" name="city" value={allData.city} />
+        <input type="hidden" name="element" value="userSubmission" />
         <input
           type="hidden"
           name="data"
@@ -129,33 +131,6 @@ export default function SubmitComponent() {
             <span className="text-sm text-red-400">{actionData.error}</span>
           </div>
         )}
-
-        <div className="space-y-4">
-          <div className="flex items-center justify-start">
-            <label className="inline-flex items-center cursor-pointer">
-              <div className="relative">
-                <input
-                  type="checkbox"
-                  checked={useToday}
-                  onChange={(e) => setUseToday(e.target.checked)}
-                  className="sr-only peer"
-                />
-                <div
-                  className="w-11 h-6 bg-white/10 border border-white/20 peer-checked:bg-green-500/20 peer-checked:border-green-500/30 rounded-full duration-200 ease-in-out transition-all"></div>
-                <div
-                  className="absolute left-[2px] top-[2px] bg-slate-100 w-5 h-5 rounded-full 
-                    duration-200 ease-in-out
-                    peer-checked:translate-x-[20px]"
-                ></div>
-              </div>
-              <span className="ml-3 text-sm font-medium text-slate-100">
-                Image from today
-              </span>
-            </label>
-          </div>
-
-          {!useToday && <div>Please ensure...</div>}
-        </div>
 
         {actionData?.success && (
           <div className="flex items-center p-4 rounded-lg bg-green-500/20 border border-green-500/30">
@@ -184,28 +159,39 @@ export default function SubmitComponent() {
           }`}
         >
           Submitting a <span className={"text-white"}>{allData.eventType}</span>{" "}
-          rated <span className={"text-white"}>{allData.rating}</span> at{" "}
-          <span className={"text-white"}>{allData.city}</span> on {" "}<span className={"text-white"}>
-  {new Date(allData.eventTime * 1000).toLocaleDateString('en-US', {
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric'
-  })}
-</span>
+          rated <span className={"text-white"}>{allData.rating}</span> on{" "}
+          <span className={"text-white"}>
+            {new Date(allData.eventTime * 1000).toLocaleDateString("en-US", {
+              month: "long",
+              day: "numeric",
+              year: "numeric",
+            })}
+          </span>{" "}
+          at <span className={"text-white"}>{allData.city}</span>
         </div>
+        {/*Future event warning*/}
+        {(allData.relative === "future" && selectedFile && !actionData?.success)&& <div className={"flex justify-center items-center text-red-400 text-sm"}>
+            <IoIosWarning className={"h-6 w-6 fill-red-500 mr-2"} />
+            This event hasnt happened yet!</div>}
+
+        {/*Relative warning*/}
+        {(allData.relative === "past" && selectedFile && !actionData?.success)&& <div className={"flex justify-center items-center text-orange-400 text-sm"}>
+            <PiWarningCircle  className={"h-6 w-6 fill-orange-500 mr-2"} />
+            This event happened {allData.eventString.split(' ').slice(1).join(' ')}</div>}
+        
         {/* Submit Button */}
         <button
           type="submit"
-          disabled={isSubmitting || !selectedFile}
+          disabled={isSubmitting || !selectedFile || allData.relative === "future"}
           className={`
             w-full px-4 py-3 rounded-lg
             font-medium text-sm
             transition-all duration-200
             ${
-            isSubmitting || !selectedFile
-              ? "bg-white/5 border-white/5 text-slate-400 cursor-not-allowed"
-              : "bg-white/20 border border-white/20 text-slate-100 hover:bg-white/30 active:bg-white/10"
-          }
+              isSubmitting || !selectedFile || allData.relative === "future"
+                ? "bg-white/5 border-white/5 text-slate-400 cursor-not-allowed"
+                : "bg-white/20 border border-white/20 text-slate-100 hover:bg-white/30 active:bg-white/10"
+            }
           `}
         >
           <div className="relative h-5">
