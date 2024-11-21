94be8dd5ef1e537031df1fdfdee32a28e41c228c
Jackson Gray <nohaxjustdoge@gmail.com>
2024-11-21 16:46:33 +0000

    refactor: comment out Datadog RUM initialization

--- app/root.tsx
+++ app/root.tsx
@@ -8,7 +8,9 @@ import {
 } from "@remix-run/react";
 
 import "./tailwind.css";
-import React from "react";
+import React from 'react';
+
+
 
 import { datadogRum } from "@datadog/browser-rum";
 
@@ -23,33 +25,33 @@ export const links: LinksFunction = () => [
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  datadogRum.init({
-    applicationId: "2d0dcfb4-e52c-4123-8899-2eb2da9ea804",
-    clientToken: "pub8caa44c4e938e0c8e06ec84e0a0bbf8e",
-    site: "us5.datadoghq.com",
-    service: "swv3",
-    env: "production",
-    sessionSampleRate: 100,
-    sessionReplaySampleRate: 100,
-    trackUserInteractions: true,
-    trackResources: true,
-    trackLongTasks: true,
-    defaultPrivacyLevel: "mask-user-input",
-  });
+  // datadogRum.init({
+  //   applicationId: "2d0dcfb4-e52c-4123-8899-2eb2da9ea804",
+  //   clientToken: "pub8caa44c4e938e0c8e06ec84e0a0bbf8e",
+  //   site: "us5.datadoghq.com",
+  //   service: "swv3",
+  //   env: "production",
+  //   sessionSampleRate: 100,
+  //   sessionReplaySampleRate: 100,
+  //   trackUserInteractions: true,
+  //   trackResources: true,
+  //   trackLongTasks: true,
+  //   defaultPrivacyLevel: "mask-user-input",
+  // });
   return (
     <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <Meta />
-        <Links />
-        <title></title>
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
+    <head>
+      <meta charSet="utf-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <Meta/>
+      <Links/>
+      <title></title>
+    </head>
+    <body>
+    {children}
+    <ScrollRestoration/>
+    <Scripts/>
+    </body>
     </html>
   );
 }
