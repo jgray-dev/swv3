ef85d45a4abe31e1114783add040865c4709540b
Jackson Gray <nohaxjustdoge@gmail.com>
2024-11-21 16:33:34 +0000

    Remove Cloudflare Turnstile script from root layout

--- app/root.tsx
+++ app/root.tsx
@@ -41,11 +41,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-        <script
-          src={"https://challenges.cloudflare.com/turnstile/v0/api.js"}
-          defer
-        ></script>
         <Meta />
         <Links />
         <title></title>
