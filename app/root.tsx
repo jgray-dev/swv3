b8e4996c0700c91a0c86b1f0d1a93c78f7575a32
Jackson Gray <nohaxjustdoge@gmail.com>
2024-11-21 16:46:39 +0000

    style: Format root.tsx and clean up whitespace

--- app/root.tsx
+++ app/root.tsx
@@ -8,9 +8,7 @@ import {
 } from "@remix-run/react";
 
 import "./tailwind.css";
-import React from 'react';
-
-
+import React from "react";
 
 import { datadogRum } from "@datadog/browser-rum";
 
@@ -40,18 +38,18 @@ export function Layout({ children }: { children: React.ReactNode }) {
   // });
   return (
     <html lang="en">
-    <head>
-      <meta charSet="utf-8"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <Meta/>
-      <Links/>
-      <title></title>
-    </head>
-    <body>
-    {children}
-    <ScrollRestoration/>
-    <Scripts/>
-    </body>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <Meta />
+        <Links />
+        <title></title>
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
     </html>
   );
 }
