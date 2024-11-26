1705d69330b6fa0dba71571207af1c6fa2402797
Jackson Gray <nohaxjustdoge@gmail.com>
2024-11-26 01:18:26 +0000

    feat: Add footer component with personal link and styling

---
+++ app/components/Footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <div
+      className={
+        "w-screen bg-transparent pt-8 text-sm text-white/25 text-center pb-4"
+      }
+    >
+      made with ♥ by{" "}
+      <a
+        href={"https://www.linkedin.com/in/jackson--gray/"}
+        target={"_blank"}
+        className={
+          "relative no-underline after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-full after:origin-left after:scale-x-0 after:bg-white/25 after:transition-transform hover:after:scale-x-100"
+        }
+      >
+        jackson
+      </a>
+    </div>)
+}
