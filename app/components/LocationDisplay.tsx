4de8b4f93181020c2ea45f7880195eda8a7080af
Jackson Gray <nohaxjustdoge@gmail.com>
2024-12-18 12:43:02 +0000

    refactor: Improve layout and add share functionality to location display

--- app/components/LocationDisplay.tsx
+++ app/components/LocationDisplay.tsx
@@ -23,34 +23,45 @@ export default function LocationDisplay() {
     <>
       <div
         className={
-          "text-center text-2xl md:text-3xl min-h-fit w-screen mx-auto h-16 text-white flex items-center -translate-y-2 justify-center gap-2 group cursor-pointer relative"
+          "text-center text-2xl md:text-3xl min-h-fit w-screen mx-auto h-16 text-white flex items-center -translate-y-2 justify-center gap-2 relative"
         }
         role="heading"
         aria-label={`Current city: ${allData.city}`}
         aria-level={1}
-        onClick={() => {
-          try {
-            void navigator.clipboard.writeText(allData.trackingLink);
-            setShowNotification(true);
-          } catch {
-            console.log(allData.trackingLink);
-            setShowNotification(true);
-          }
-        }}
       >
-        <span>{allData.city}</span>
-        <IoIosLink
-          className={
-            "w-6 h-6 fill-gray-500 group-hover:fill-gray-300 duration-500 translate-y-0.5"
-          }
-          title={"Copy link to this page"}
-        />
+        <div
+          className="flex items-center gap-2 group cursor-pointer"
+          onClick={() => {
+            try {
+              if (!showNotification) {
+                void navigator.clipboard.writeText(allData.trackingLink);
+                setShowNotification(true);
+              } else {
+                void navigator.share({
+                  title: "test title",
+                  url: "https://google.com",
+                });
+              }
+            } catch {
+              console.log(allData.trackingLink);
+              setShowNotification(true);
+            }
+          }}
+        >
+          <span>{allData.city}</span>
+          <IoIosLink
+            className={
+              "w-6 h-6 fill-gray-500 group-hover:fill-gray-300 duration-500 translate-y-0.5"
+            }
+            title={"Copy link to this page"}
+          />
+        </div>
 
         {showNotification && (
           <div
             className="absolute -bottom-10 left-1/2 transform -translate-x-1/2 
-                     bg-black/50 text-white backdrop-blur-sm text-sm py-2 px-4 rounded-md
-                     shadow-lg animate-[fadeInOut_2s_ease-in-out]"
+                   bg-black/50 text-white backdrop-blur-sm text-sm py-2 px-4 rounded-md
+                   shadow-lg animate-[fadeInOut_2s_ease-in-out]"
           >
             Link copied to clipboard
           </div>
